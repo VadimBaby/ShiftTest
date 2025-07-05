50e398e859c5142ee2e0f5095c7b5041668bb5bd
VadimBaby <vadimdim22@gmail.com>
2025-07-06 00:54:09 +0700

    updated scheme

--- Workspace.swift
+++ Workspace.swift
@@ -3,5 +3,6 @@ import ProjectDescriptionHelpers
 
 let workspace = Workspace(
     name: "Workspace",
-    projects: Constants.Modules.allPaths()
+    projects: Constants.Modules.allPaths(),
+    schemes: [.scheme(name: "MAIN", buildAction: .buildAction(targets: [.project(path: "//Projects/ShiftTest", target: "ShiftTest")]))]
 )
